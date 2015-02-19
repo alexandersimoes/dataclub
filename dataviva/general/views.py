4b9d19970286b7279d9c00963cf353249b5c69fb
Jon Speiser <jspeis@gmail.com>
2015-02-19 08:57:19 -0500

    homepage cache

--- dataviva/general/views.py
+++ dataviva/general/views.py
@@ -131,7 +131,7 @@ def after_request(response):
     return response
 
 @mod.route('/', methods=['GET', 'POST'])
-@view_cache.cached(timeout=604800, key_prefix=make_cache_key)
+@view_cache.cached(key_prefix=make_cache_key)
 def home():
     g.page_type = "home"
     lists = []
